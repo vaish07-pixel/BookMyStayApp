9755e880b39c00909be264eb83bfd2a2f9ddc902
vaish07-pixel <vsgithub07@gmail.com>
2026-03-13 15:11:41 +0530

    UC5:Booking Request

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,30 +1,65 @@
-class Room {
-    private String type;
-    private double price;
-    private String amenities;
+public class Reservation {
 
-    public Room(String type, double price, String amenities) {
-        this.type = type;
-        this.price = price;
-        this.amenities = amenities;
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
     }
 
-    public String getType() {
-        return type;
+    public String getGuestName() {
+        return guestName;
     }
 
-    public double getPrice() {
-        return price;
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BookingRequestQueue {
+
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
     }
 
-    public String getAmenities() {
-        return amenities;
+    // Add booking request
+    public void addRequest(Reservation reservation) {
+        requestQueue.add(reservation);
+        System.out.println("Booking request added for: " + reservation.getGuestName());
     }
 
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + type);
-        System.out.println("Price: $" + price);
-        System.out.println("Amenities: " + amenities);
-        System.out.println("-------------------------");
+    // View all requests
+    public void displayRequests() {
+        System.out.println("\nBooking Requests in Queue:");
+
+        for (Reservation r : requestQueue) {
+            System.out.println(r.getGuestName() + " requested " + r.getRoomType() + " room");
+        }
     }
-}}
+}
+
+public class UseCase5BookingRequest {
+
+    public static void main(String[] args) {
+
+        BookingRequestQueue queue = new BookingRequestQueue();
+
+        Reservation r1 = new Reservation("Alice", "Single");
+        Reservation r2 = new Reservation("Bob", "Double");
+        Reservation r3 = new Reservation("Charlie", "Suite");
+
+        // Guests submit requests
+        queue.addRequest(r1);
+        queue.addRequest(r2);
+        queue.addRequest(r3);
+
+        // Display queued requests
+        queue.displayRequests();
+    }
+}
