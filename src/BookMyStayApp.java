97bba06f553d680797ea3b8b411cf21e0a501f94
vaish07-pixel <vsgithub07@gmail.com>
2026-03-13 15:17:41 +0530

    UC4:Room Search and availability check

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -28,3 +28,76 @@ class Room {
         System.out.println("-------------------------");
     }
 }}
+
+        import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+
+    private Map<String, Integer> availability = new HashMap<>();
+
+    public RoomInventory() {
+        availability.put("Single", 3);
+        availability.put("Double", 2);
+        availability.put("Suite", 0);
+    }
+
+    public Map<String, Integer> getRoomAvailability() {
+        return availability;
+    }
+}
+
+import java.util.Map;
+
+public class RoomSearchService {
+
+    public void searchAvailableRooms(
+            RoomInventory inventory,
+            Room singleRoom,
+            Room doubleRoom,
+            Room suiteRoom) {
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        System.out.println("Available Rooms:");
+        System.out.println("================");
+
+        // Single Room
+        if (availability.get("Single") > 0) {
+            singleRoom.displayRoomDetails();
+        }
+
+        // Double Room
+        if (availability.get("Double") > 0) {
+            doubleRoom.displayRoomDetails();
+        }
+
+        // Suite Room
+        if (availability.get("Suite") > 0) {
+            suiteRoom.displayRoomDetails();
+        }
+    }
+}
+public class UseCase4RoomSearch {
+
+    public static void main(String[] args) {
+
+        // Create Room Objects
+        Room singleRoom = new Room("Single", 100, "WiFi, TV, Single Bed");
+        Room doubleRoom = new Room("Double", 180, "WiFi, TV, Double Bed");
+        Room suiteRoom = new Room("Suite", 300, "WiFi, TV, King Bed, Mini Bar");
+
+        // Create Inventory
+        RoomInventory inventory = new RoomInventory();
+
+        // Search Service
+        RoomSearchService searchService = new RoomSearchService();
+
+        // Perform Search
+        searchService.searchAvailableRooms(
+                inventory,
+                singleRoom,
+                doubleRoom,
+                suiteRoom);
+    }
+}
